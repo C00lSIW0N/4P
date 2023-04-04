c38763162cc40a7fa74094c6760a8feba6b9c8fd
C00lSIW0N <siwon6911@naver.com>
2023-04-04 14:50:02 +0900

    4/4 파베 테스트
    테스트중

--- 4P/src/App.js
+++ 4P/src/App.js
@@ -1,13 +1,24 @@
 // import NavPage from "./components/NavPage";
-import { Route, Routes } from "react-router-dom";
-import Main from './components/MainPage';
+// import { Route, Routes } from "react-router-dom";
+// import Main from './components/MainPage';
+import { getFirestore } from "./firebase-config.js";
+import { useEffect } from "react";
 
-const App = () => (
-  <Routes>
-  <Route path='/' element={<Main />}/>
-  <Route path='/main' element={<Main />}/>
-  {/* <Route path='*' element={<Error />}/> */}
-</Routes>
-
-);
+const App = () => {
+  useEffect(()=> {
+    console.log(getFirestore);
+  })
+  return(
+    <div className="App">
+      firebase 확인해보기!
+    </div>
+//   <Routes>
+//   <Route path='/' element={<Main />}/>
+//   <Route path='/main' element={<Main />}/>
+//   {/* <Route path='*' element={<Error />}/> */}
+// </Routes>
+//   );
+  );
+  };
 export default App;
+
