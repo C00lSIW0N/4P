2852a0b174089b2f2c377a294e11e706284f59ed
C00lSIW0N <siwon6911@naver.com>
2023-04-06 18:33:37 +0900

    4/6
    파이어베이스 데이터 추가 성공

--- 4P/src/components/MainPage.js
+++ 4P/src/components/MainPage.js
@@ -1,14 +1,16 @@
-import React from "react";
-import { Link } from "react-router-dom";
+// import React, { useEffect, useState } from "react";
+// import { Link } from "react-router-dom";
+// import { firestore } from "../firebase_config";
+// import { firebase } from "../firebase_config";
 
 
-export const Main = () => {
-  return (
-    <div className='main'>
-      <h1>ㅎㅇ요</h1>
-      <h1><Link to='navpage'>zz</Link></h1>
-    </div>
-  );
-}
- 
-export default Main;
+// export const Main = () => {
+//   return (
+//     <div className='main'>
+//       <h1>ㅎㅇ요</h1>
+//       <h1><Link to='navpage'>zz</Link></h1>
+//     </div>
+//   );
+//   }
+
+// export default Main;
