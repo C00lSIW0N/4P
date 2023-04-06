d5083dcfa22e93f33432845133b6ddbce18e5fdf
C00lSIW0N <siwon6911@naver.com>
2023-04-06 18:33:37 +0900

    4/6
    파이어베이스 데이터 추가 성공

--- 4P/src/App.js
+++ 4P/src/App.js
@@ -1,24 +1,44 @@
 // import NavPage from "./components/NavPage";
 // import { Route, Routes } from "react-router-dom";
-// import Main from './components/MainPage';
-import { getFirestore } from "./firebase-config.js";
-import { useEffect } from "react";
+//  import Main from './components/MainPage';
+// import { getFirestore } from "./firebase_config.js";
+// import { useEffect } from "react";
 
-const App = () => {
-  useEffect(()=> {
-    console.log(getFirestore);
-  })
-  return(
-    <div className="App">
-      firebase 확인해보기!
-    </div>
+// const App = () => {
+//   // useEffect(()=> {
+//   //   console.log(getFirestore);
+//   // })
+//   // return(
+//   //   <div className="App">
+//   //     firebase 확인해보기!
+//   //   </div>
 //   <Routes>
 //   <Route path='/' element={<Main />}/>
 //   <Route path='/main' element={<Main />}/>
 //   {/* <Route path='*' element={<Error />}/> */}
 // </Routes>
-//   );
-  );
-  };
-export default App;
+//   // );
+//   // );
+//   };
+// export default App;
 
+// import './App.css';
+import { firestore } from './firebase_config';
+import { useEffect } from 'react';
+
+function App() {
+
+  useEffect(() => {
+    const test = firestore.collection("test");
+    
+    test.doc("hi").set({test: 'hello'});
+  });
+
+  return (
+    <div className="App">
+      firebase 확인해보기!
+    </div>
+  );
+}
+
+export default App;
--- 4P/src/firebase_config.js
+++ 4P/src/firebase_config.js
@@ -1,7 +1,13 @@
 // Import the functions you need from the SDKs you need
-import { initializeApp } from "firebase/app";
-import { getAnalytics } from "firebase/analytics";
-import { getFirestore } from "firebase/firestore";
+// import { initializeApp } from "firebase/app";
+// import { getAnalytics } from "firebase/analytics";
+// import { getFirestore } from "firebase/firestore";
+// import firebase from "firebase";
+import firebase from 'firebase/compat/app';
+// import { initializeApp } from "firebase/app";
+import 'firebase/compat/firestore';
+
+
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 
@@ -18,9 +24,10 @@ const firebaseConfig = {
 };
 
 // Initialize Firebase
-const app = initializeApp(firebaseConfig);
-const analytics = getAnalytics(app);
+// const app = initializeApp(firebaseConfig);
+// const analytics = getAnalytics(app);
 // const firestore = firebase.firestore();
 
 
-export { getFirestore };
+const firebaseApp = firebase.initializeApp(firebaseConfig);
+export const firestore = firebaseApp.firestore();
