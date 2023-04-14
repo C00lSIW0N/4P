0c37bdf706680bc3fb0581d181d06712781ab345
C00lSIW0N <siwon6911@naver.com>
2023-04-14 13:25:18 +0900

    4/14
    결제창 띄우기

--- 4P/src/components/Payment.js
+++ 4P/src/components/Payment.js
@@ -22,60 +22,117 @@
 // function Payment () {
   
 	
-// 	useEffect(() => {
-//         const jquery = document.createElement("script");
-//         jquery.src = "https://code.jquery.com/jquery-1.12.4.min.js";
-//         const iamport = document.createElement("script");
-//         iamport.src = "https://cdn.iamport.kr/v1/iamport.js";
-//         document.head.appendChild(jquery);
-//         document.head.appendChild(iamport);
-//         return () => {
-//         	document.head.removeChild(jquery);
-//         	document.head.removeChild(iamport);
-//         }
-//     }, []);
 
-//     const onClickPayment = () =>{
-// 		var IMP = window.IMP;
-//     	IMP.init('imp02656648');
+  //    []);
 
-//     IMP.request_pay ({
-//     		pg: 'kakopay.TC0ONETIME',
-//     		pay_method: 'card',
-//     		merchant_uid: `mid_${new Date().getTime()}`,
-//     		name: '테스트',
-//     		amount: '1000',
-//     		custom_data: {
-//                 name: '부가정보',
-//                 desc: '세부 부가정보'
-//     		},
-//     		buyer_name: '홍길동',
-//     		buyer_tel: '027604114',
-//     		buyer_email: 'hansung@hansung.ac.kr',
-//     		buyer_addr: '성북구 삼선교로 16길 116',
-//     		buyer_postalcode: '02876'
-//     	},
-//     function callback (rsp) {
-//     	if (rsp.success){
-//     		alert('결제 성공');
-//     	} else {
-//     		alert(`결제 실패: ${rsp.error_msg}`);
-//     	}
-//     });
-// 	const response = () => {
-// 		if(response.success) {
-// 			alert('결제 성공');
-// 		}
-// 		else{
-//			alert('결제 실패: ${error_msg}');
-// 	}
-// 	}
+    // const onClickPayment = () =>{
+		// var IMP = window.IMP;
+    // 	IMP.init('imp02656648');
 
-//     return (
-//         <>
-// 		{onClickPayment()};
-//         </>
-//     );
+  //   IMP.request_pay ({
+  //   		pg: 'kakopay.TC0ONETIME',
+  //   		pay_method: 'card',
+  //   		merchant_uid: `mid_${new Date().getTime()}`,
+  //   		name: '테스트',
+  //   		amount: '1000',
+  //   		custom_data: {
+  //               name: '부가정보',
+  //               desc: '세부 부가정보'
+  //   		},
+  //   		buyer_name: '홍길동',
+  //   		buyer_tel: '027604114',
+  //   		buyer_email: 'hansung@hansung.ac.kr',
+  //   		buyer_addr: '성북구 삼선교로 16길 116',
+  //   		buyer_postalcode: '02876'
+  //   	},
+  //   function callback (rsp) {
+  //   	if (rsp.success){
+  //   		alert('결제 성공');
+  //   	} else {
+  //   		alert(`결제 실패: ${rsp.error_msg}`);
+  //   	}
+  //   });
+	// const response = () => {
+	// 	if(response.success) {
+	// 		alert('결제 성공');
+	// 	}
+	// 	else{
+	// 		alert('결제 실패: ${error_msg}');
+	// }
+	// }
+
+    // return (
+    //     <>
+		// {onClickPayment()};
+    //     </>
+    // );
 // }
 // }
 // export default Payment;
+
+import React from "react";
+import { useEffect } from "react";
+import { Link } from "react-router-dom";
+
+ 
+const Payment = (effect, deps) => {
+
+  useEffect(() => {
+    const jquery = document.createElement("script");
+    jquery.src = "https://code.jquery.com/jquery-1.12.4.min.js";
+    const iamport = document.createElement("script");
+    iamport.src = "https://cdn.iamport.kr/js/iamport.payment-1.1.7.js";
+    document.head.appendChild(jquery);
+    document.head.appendChild(iamport);
+    return () => {
+      document.head.removeChild(jquery);
+      document.head.removeChild(iamport);
+    }},
+  []);
+  const Pay = () => {
+    const { IMP } = window;
+    IMP.init('imp02656648');
+
+      const data = {
+    		pg: 'kakopay.TC0ONETIME',
+    		pay_method: 'card',
+    		merchant_uid: `mid_${new Date().getTime()}`,
+    		name: '테스트',
+    		amount: '1000',
+    		custom_data: {
+            name: '부가정보',
+            desc: '세부 부가정보'
+    		},
+    		buyer_name: '홍길동',
+    		buyer_tel: '027604114',
+    		buyer_email: 'hansung@hansung.ac.kr',
+    		buyer_addr: '성북구 삼선교로 16길 116',
+    		buyer_postalcode: '02876'
+    	};
+      IMP.request_pay(data,callback);
+    }
+      const callback = (rsp) => {
+        const {success, error_msg, imp_uid, merchant_uid, pay_method, paid_amount, status} = rsp;
+    	if (success){
+    		alert('결제 성공');
+    	} else {
+    		alert(`결제 실패: ${error_msg}`);
+    	}
+    }
+	// const response = () => {
+	// 	if(response.success) {
+	// 		alert('결제 성공');
+	// 	}
+	// 	else{
+	// 		alert('결제 실패: ${error_msg}');
+	// }
+	// }
+  return (
+    <>
+    <button onClick={Pay}>결제하기</button>
+    </>
+);
+}
+
+ 
+export default Payment;
