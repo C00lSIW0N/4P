7b15efd208a0a1554b7c73b97f24dc6ffc37e507
aneunoun <ththgy@naver.com>
2023-04-14 12:44:00 +0900

    ?

--- 4P/src/components/Reservation/ReContents.js
+++ 4P/src/components/Reservation/ReContents.js
@@ -2,28 +2,7 @@
 
 
 function ReContents(){
-    const body ={
-        display:"flex",
-        flexDirection:"row",
-        width:"100%"
-      }
-      const secLeft={
-        width:"100px"
-      }
-      const secRight={
-        width:"100px"
-      }
-      const secCenter={
-        width:"100%",
-        height:"100%",
-        display:"flex",
-        flexDirection:"column"
-      }
-
-      return(
-        <>
-        </>
-      );
+    
 }
 
 export default ReContents;
