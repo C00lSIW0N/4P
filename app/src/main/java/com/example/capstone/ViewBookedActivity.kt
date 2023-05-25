9b3480aa48cfcb2929ed2a2aef17dd27946bca26
010101Min <117823790+010101Min@users.noreply.github.com>
2023-05-26 08:35:22 +0900

    예약 내역 선택해서 추가 기능 추가 (아직 필터링 기능은 없음), 장바구니 내역도 추가해야 함

---
+++ app/src/main/java/com/example/capstone/ViewBookedActivity.kt
@@ -0,0 +1,61 @@
+package com.example.capstone
+
+import android.content.Intent
+import android.os.Bundle
+import android.widget.Button
+import androidx.appcompat.app.AppCompatActivity
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.firestore.FirebaseFirestore
+import com.google.firebase.firestore.ktx.firestore
+import com.google.firebase.ktx.Firebase
+
+class ViewBookedActivity : AppCompatActivity() {
+    private lateinit var recyclerView: RecyclerView
+    private lateinit var adapter: ViewBooked
+    private lateinit var dataList: MutableList<Booked>
+    private lateinit var backtotimetable: Button
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_booked)
+
+        recyclerView = findViewById(R.id.recyclerView)
+        val layoutManager = LinearLayoutManager(this)
+        recyclerView.layoutManager = layoutManager
+
+        dataList = mutableListOf()
+
+        val db: FirebaseFirestore = Firebase.firestore
+        val tempuser = FirebaseAuth.getInstance().currentUser
+        var useremail = "email"
+        if (tempuser != null) { useremail = tempuser.email.toString() }
+
+        val bookRef = db.collection("user")
+        val bookDocument = bookRef.document(useremail)
+
+        bookDocument.get()
+            .addOnSuccessListener { document ->
+                if (document != null && document.exists()) {
+
+                    val name = document.getString("숙소") ?: ""
+                    val category = document.getString("구분") ?: ""
+                    val daytime = document.getString("예약날짜") ?: ""
+
+                    val booked = Booked(name, category, daytime)
+                    dataList.add(booked)
+
+                    adapter = ViewBooked(dataList)
+                    recyclerView.adapter = adapter
+                }
+            }
+            .addOnFailureListener { exception -> }
+
+        backtotimetable = findViewById<Button>(R.id.backtotimetable)
+        backtotimetable.setOnClickListener {
+            var intent = Intent(this, TimeTableActivity::class.java)
+            startActivity(intent)
+        }
+    }
+}
